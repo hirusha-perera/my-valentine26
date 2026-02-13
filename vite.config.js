b35d7792a91a3f6caa6acfc69725c0431ae8628e
iamhirusha <hirushaperera98@gmail.com>
2026-02-14 00:29:27 +0530

    fix: updated base and relative paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-valentine26/', 
+  base: '/my-valentine26/', // This MUST match your repo name exactly with slashes
 })
